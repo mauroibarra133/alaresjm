872e749d59c8c5da37b26832f5635b82d3e7709d
Mauro Ibarra <mauroibarra133@gmail.com>
2023-08-26 22:53:39 -0300

    modify(backend): add some changes

--- App/Backend/dist/index.js
+++ App/Backend/dist/index.js
@@ -1,11 +1,10 @@
+import app from '../app';
 
-
-import _interopRequireDefault from "@babel/runtime/helpers/interopRequireDefault";
-var _app = _interopRequireDefault(require("../app"));
-//Controlar rutas
-_app["default"].get('/', function (req, res) {
+// Controlar rutas
+app.get('/', function (req, res) {
   res.send('Backend Iniciado');
 });
-_app["default"].listen(_app["default"].get('port'), function () {
-  console.log('Sitio escuchando en el puerto', _app["default"].get('port'));
-}); 
+
+app.listen(app.get('port'), function () {
+  console.log('Sitio escuchando en el puerto', app.get('port'));
+});
